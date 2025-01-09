9a2402efa91824ef0440f8b77ede5f1fdef1d6ce
Andrey Kurudimov <andreykurudimov@gmail.com>
2025-01-09 00:38:07 -0800

    [bug] fixed incorrect return type

--- src/commands/seed/list.command.ts
+++ src/commands/seed/list.command.ts
@@ -3,7 +3,7 @@ import { SeedManager } from '../../components/seed.manager';
 /**
  * Wherever the seed directory is, we will print out the folders and files in it the way the tree command does
  */
-export const listSeeds = async (dirPath?: string): Promise<void> => {
+export const listSeeds = (dirPath?: string): void => {
     const subNode = dirPath ? SeedManager.getFile(dirPath) : undefined;
     SeedManager.printTree(subNode);
 };
