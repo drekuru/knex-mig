367bf36ba807b83c38886c3522045cdb2ab9fdce
Andrey Kurudimov <andrey@trukt.com>
2025-01-11 15:55:04 -0800

    [bug/unsafe-path-read] fixed error thrown without mg setup

--- src/components/path.manager.ts
+++ src/components/path.manager.ts
@@ -3,19 +3,49 @@ import fs from 'fs';
 import { pp } from '../utils';
 
 export class PathManager {
-    static USER_HOME = process.env.USERPROFILE || process.env.HOME || '';
-    static GENERAL_CONFIGS_DIR_PATH = path.join(this.USER_HOME, '.config');
-    static MIG_DIR_PATH = path.join(this.GENERAL_CONFIGS_DIR_PATH, 'knex-mig');
-    static ENVS_DIR_PATH = path.join(this.MIG_DIR_PATH, 'envs');
-    static CONTEXT_FILE_PATH = path.join(this.MIG_DIR_PATH, 'context.json');
+    private static _USER_HOME = process.env.USERPROFILE || process.env.HOME || '';
+    private static _GENERAL_CONFIGS_DIR_PATH = path.join(this._USER_HOME, '.config');
+    private static _MIG_DIR_PATH = path.join(this._GENERAL_CONFIGS_DIR_PATH, 'knex-mig');
+    private static _ENVS_DIR_PATH = path.join(this._MIG_DIR_PATH, 'envs');
+    private static _CONTEXT_FILE_PATH = path.join(this._MIG_DIR_PATH, 'context.json');
 
     static setupPaths(): void {
-        if (!fs.existsSync(PathManager.GENERAL_CONFIGS_DIR_PATH)) fs.mkdirSync(PathManager.GENERAL_CONFIGS_DIR_PATH);
-        if (!fs.existsSync(PathManager.MIG_DIR_PATH)) fs.mkdirSync(PathManager.MIG_DIR_PATH);
-        if (!fs.existsSync(PathManager.ENVS_DIR_PATH)) fs.mkdirSync(PathManager.ENVS_DIR_PATH);
-        if (!fs.existsSync(PathManager.CONTEXT_FILE_PATH))
-            fs.writeFileSync(PathManager.CONTEXT_FILE_PATH, JSON.stringify({ envName: 'default' }, null, 4));
+        if (!fs.existsSync(PathManager._GENERAL_CONFIGS_DIR_PATH)) fs.mkdirSync(PathManager._GENERAL_CONFIGS_DIR_PATH);
+        if (!fs.existsSync(PathManager._MIG_DIR_PATH)) fs.mkdirSync(PathManager._MIG_DIR_PATH);
+        if (!fs.existsSync(PathManager._ENVS_DIR_PATH)) fs.mkdirSync(PathManager._ENVS_DIR_PATH);
+        if (!fs.existsSync(PathManager._CONTEXT_FILE_PATH))
+            fs.writeFileSync(PathManager._CONTEXT_FILE_PATH, JSON.stringify({ envName: 'default' }, null, 4));
 
-        pp.debug(`Setup knex-mig at ${PathManager.MIG_DIR_PATH}`);
+        pp.debug(`Setup knex-mig at ${PathManager._MIG_DIR_PATH}`);
+    }
+
+    private static safeGetPath(pathToCheck: string): string {
+        if (!fs.existsSync(pathToCheck)) {
+            pp.error(`Path ${pathToCheck} does not exist`);
+            pp.warn('Please run `mg setup`');
+            process.exit(1);
+        }
+
+        return pathToCheck;
+    }
+
+    static get USER_HOME(): string {
+        return this.safeGetPath(this._USER_HOME);
+    }
+
+    static get GENERAL_CONFIGS_DIR_PATH(): string {
+        return this.safeGetPath(this._GENERAL_CONFIGS_DIR_PATH);
+    }
+
+    static get MIG_DIR_PATH(): string {
+        return this.safeGetPath(this._MIG_DIR_PATH);
+    }
+
+    static get ENVS_DIR_PATH(): string {
+        return this.safeGetPath(this._ENVS_DIR_PATH);
+    }
+
+    static get CONTEXT_FILE_PATH(): string {
+        return this.safeGetPath(this._CONTEXT_FILE_PATH);
     }
 }
